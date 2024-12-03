49e0282d3504634f27ff0bb1ad3885d75ebc86a8
Levy Aragao <levyaragao@MacBook-Air-de-Levy.local>
2024-12-03 13:52:52 -0300

    finalizado query de update e delete no postgres

--- database _postgres.js
+++ database _postgres.js
@@ -26,12 +26,15 @@ export class Database {
 
 
 
-    update(id, video) {
-
+    async update(id, video) {
+        const {title, description, duration} = video
+         await sql`UPDATE videos SET title = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id}`
+         
 
     }
 
-    delete(id) {
+  async  delete(id) {
+        return await sql`DELETE FROM videos WHERE id = ${id}`
 
     }
 
